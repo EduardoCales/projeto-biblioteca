6080a72f8f8e3ba54a3173b7f724f635b74ecf6d
Eduardo Cales Ferreira <dudu.calesferreira@gmail.com>
2024-07-12 02:10:23 -0300

    Arrumei o jeito de exibir o nome do author

--- bibliotech/models.py
+++ bibliotech/models.py
@@ -1,11 +1,10 @@
 from django.db import models
 
 class Author(models.Model):
-    first_name = models.CharField(max_length=65)
-    last_name = models.CharField(max_length=65)
+    name = models.CharField(max_length=65)
 
     def __str__ (self):
-        return self.first_name
+        return self.name
     
 class Category(models.Model):
     name = models.CharField(max_length=65)
