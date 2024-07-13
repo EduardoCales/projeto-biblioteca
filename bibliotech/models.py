509d1eacb34181ec6ba2294e0f4d96fa2153ba8a
Eduardo Cales Ferreira <dudu.calesferreira@gmail.com>
2024-07-13 00:27:17 -0300

    add o campo de quantidade de livros

--- bibliotech/models.py
+++ bibliotech/models.py
@@ -17,12 +17,24 @@ class Livro(models.Model):
     description = models.CharField(max_length=500)
     slug = models.SlugField()
     cover = models.ImageField(upload_to='bibliotech/covers/%Y/%m/%d/')
+    quantidade = models.DecimalField(max_digits=15, decimal_places=2)
     category = models.ForeignKey(
         Category, on_delete=models.SET_NULL, null=True
     )
     author = models.ForeignKey(
         Author, on_delete=models.SET_NULL, null=True
     )
+    """
+    user = models.ForenKey(
+        User, on_delete=models.SET_NULL, null=True)
+    """
 
     def __str__ (self):
         return self.title
+    
+"""
+class Emprestimo(models.Model):
+    user = models.ForenKey(
+        User, on_delete=models.SET_NULL, null=True)
+    data_retirada = models.DateTime()
+"""
