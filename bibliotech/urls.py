a5d7a68c7fb79779b6554d21de946766cd5525ff
Eduardo Cales Ferreira <dudu.calesferreira@gmail.com>
2024-07-13 14:25:10 -0300

    criando uma nova view para pesquisa

--- bibliotech/urls.py
+++ bibliotech/urls.py
@@ -6,6 +6,7 @@ app_name = 'livros'
 
 urlpatterns = [
     path('', views.home, name="home"),
+    path('livros/search/', views.search, name="search"),
     path('livros/category/<int:category_id>/', views.category, name="category"),
     path('livros/<int:id>/', views.livro, name="livro"),
 ]
--- bibliotech/views.py
+++ bibliotech/views.py
@@ -1,3 +1,4 @@
+from django.http import Http404
 from django.shortcuts import get_list_or_404, get_object_or_404, render
 
 from bibliotech.models import Livro
@@ -28,3 +29,12 @@ def livro(request, id):
         'is_detail_page': True,
     })
 
+def search(request):
+    search_term = request.GET.get('q', '').strip()
+
+    if not search_term:
+        raise Http404
+
+    return render(request, 'bibliotech/pages/search.html', context={
+        'page_title': f'Search for "{search_term}" |'
+    })
